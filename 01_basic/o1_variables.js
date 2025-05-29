68a7cd3c898b9a133aab471d0c5bd370ed3f1bb7
bhatiminu <40340277+bhatiminu@users.noreply.github.com>
2025-05-29 05:31:17 +0000

    data types

---
+++ 01_basic/o1_variables.js
@@ -0,0 +1,11 @@
+const userId = 2345;
+let email = "mnksh@gmail.com";
+var address = "adarsh colony";
+let city ;
+//userId = 568;
+email = "bhati@gmail.com";
+address = "pulla colony";
+// city = udaipur;
+ console.log(userId);
+ console.table([email, address, city]);
+//  not to use var beause of its block scop 
