6c74d3a2f2757995e959848903dac833988679dc
bhatiminu <40340277+bhatiminu@users.noreply.github.com>
2025-06-25 11:35:50 +0000

    done promises

--- 09_advance/promise.js
+++ 09_advance/promise.js
@@ -1,100 +1,134 @@
-const promiseOne = new Promise(function(resolve, reject){
-    // do an async task 
-    setTimeout(function() {
-        console.log("Meenakshi");
-        resolve()
-    },1000)
-})
-// consume krna 
-promiseOne.then(function() {
-    console.log('promise consumed');
+// const promiseOne = new Promise(function(resolve, reject){
+//     // do an async task 
+//     setTimeout(function() {
+//         console.log("Meenakshi");
+//         resolve()
+//     },1000)
+// })
+// // consume krna 
+// promiseOne.then(function() {
+//     console.log('promise consumed');
     
-})
+// })
 
 
-new Promise(function(resolve, reject){
-setTimeout(function(){
-    console.log("Second task");
-    resolve()
-},1000)
-}).then(function(){
-    console.log('promise consumed second');
+// new Promise(function(resolve, reject){
+// setTimeout(function(){
+//     console.log("Second task");
+//     resolve()
+// },1000)
+// }).then(function(){
+//     console.log('promise consumed second');
     
-})
+// })
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username:'minu'})
-    },1000)
-})
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username:'minu'})
+//     },1000)
+// })
 
-promiseThree.then(function(user){
-    console.log(user);
+// promiseThree.then(function(user){
+//     console.log(user);
     
-})
+// })
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if(!error) {
-          resolve({username: "Minu", password: "123"})  
-        }
-        else{
-            reject('Error coming')
-        }
-    },1000)
-})
-// chaining 
-promiseFour.then((user) => {
-console.log(user);
-return user.username
-}).then((username) => {
-    console.log(username);
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if(!error) {
+//           resolve({username: "Minu", password: "123"})  
+//         }
+//         else{
+//             reject('Error coming')
+//         }
+//     },1000)
+// })
+// // chaining 
+// promiseFour.then((user) => {
+// console.log(user);
+// return user.username
+// }).then((username) => {
+//     console.log(username);
     
-}).catch((err) => {
-    console.log(err);
-}).finally(() => console.log('finally')) // allways exicuit 
+// }).catch((err) => {
+//     console.log(err);
+// }).finally(() => console.log('finally')) // allways exicuit 
 
-const promiseFive = new Promise(function(resolve, reject){
-       setTimeout(function(){
-        let error = true
-        if(!error) {
-          resolve({username: "java script", password: "123"})  
-        }
-        else{
-            reject('Error coming java script')
-        }
-    },1000)
-})
+// const promiseFive = new Promise(function(resolve, reject){
+//        setTimeout(function(){
+//         let error = true
+//         if(!error) {
+//           resolve({username: "java script", password: "123"})  
+//         }
+//         else{
+//             reject('Error coming java script')
+//         }
+//     },1000)
+// })
 
-async function consumepromiseFive() {
-    try {
-        const response = await promiseFive
-        console.log(response);  
-    } catch (error) {
-        console.log(error);
-    }
-}
-consumepromiseFive()
-
-// async function getallUser() {
+// async function consumepromiseFive() {
 //     try {
-//   const response =   await fetch('https://api.github.com/users/bhatiminu')
-//   const data = await response.json()
-//   console.log(data);
-  
+//         const response = await promiseFive
+//         console.log(response);  
 //     } catch (error) {
 //         console.log(error);
-        
 //     }
 // }
-// getallUser()
+// consumepromiseFive()
 
-fetch('https://api.github.com/users/bhatiminu')
-.then((response) => {
+// // async function getallUser() {
+// //     try {
+// //   const response =   await fetch('https://api.github.com/users/bhatiminu')
+// //   const data = await response.json()
+// //   console.log(data);
+  
+// //     } catch (error) {
+// //         console.log(error);
+        
+// //     }
+// // }
+// // getallUser()
+
+// fetch('https://api.github.com/users/bhatiminu')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => console.log(error))
+
+//
+const url = "https://jsonplaceholder.typicode.com/users/1";
+/** 
+  // Sample Response
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org"
+  } 
+**/
+fetch(url).then(response => {
     return response.json()
+}).then((data) => {
+    console.log("User Data:",data);
+    
+}).catch((err) => {
+    console.log("Fetch error:",err);
+    
 })
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => console.log(error))
+
+function counter() {
+    const[count, setcount] = setstate(0);
+
+    const inc = () => {
+        setcount(count + 1)
+    };
+    const dec = () => {
+        setcount(count - 1)
+    }
+}
